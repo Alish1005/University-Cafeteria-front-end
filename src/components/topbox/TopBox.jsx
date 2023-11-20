0fa8fea91177743cc093145419590d94c9a9e8f6
Alish313 <102430993+Alish313@users.noreply.github.com>
2023-11-21 01:34:28 +0200

    dashboard connect to database

--- src/components/topbox/TopBox.jsx
+++ src/components/topbox/TopBox.jsx
@@ -5,7 +5,7 @@ import React from "react";
 function TopBox(props) {
     const formatter = (value) => <div className="d-flex text-primary fw-bold"><CountUp end={value} separator="," className='' /><p>{props.add}</p></div>;
     return ( 
-        <div className={`${props.isNormal ? "TopBox bg-secondary ":" bg-info "}text-primary border shadow-sm col-lg-2 col-md-4 col-sm-4 m-3 p-2`}>
+        <div className={`${props.isNormal ? "TopBox bg-secondary ":"TopBox2 bg-info "}text-primary border shadow-sm col-lg-2 col-md-4 col-sm-4 m-3 p-2`}>
             <h5>{props.title}</h5>
             <div className="d-flex justify-content-between m-3">
                 {props.name!=null && <h5 className="text-center mt-2">{props.name}</h5>}
